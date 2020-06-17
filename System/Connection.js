0aa39595a3c56cc4634f4df18669202beae13c6f
uutkukorkmaz <uutkukorkmaz@gmail.com>
2020-06-17 22:51:08 +0300

    initial commit

--- System/Connection.js
+++ System/Connection.js
@@ -14,8 +14,8 @@ module.exports = class Connection {
         let server = connection.server;
         let player = connection.player;
 
-        socket.emit('spawn',player);
-        socket.broadcast.emit('spawn',player);
+        socket.emit('spawn',player.data);
+        socket.broadcast.emit('spawn',player.data);
 
         socket.on('disconnect', () => {
             server.onDisconnected(connection);
