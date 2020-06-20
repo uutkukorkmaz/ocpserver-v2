a5d719609168846523f6c8e31cc8a2d5e96163a9
uutkukorkmaz <uutkukorkmaz@gmail.com>
2020-06-21 00:08:14 +0300

    Player position update

--- System/Connection.js
+++ System/Connection.js
@@ -16,13 +16,11 @@ module.exports = class Connection {
         let player = connection.player;
 
 
-        //socket.emit('spawn', player);
-        //console.log("[SERVER]: "+player.id+" spawned ");
+
         socket.broadcast.emit('spawn', player);
         for(let playerID in server.players){
             if(server.players[playerID].id != player.id) {
                 socket.emit('spawn', server.players[playerID]);
-               // console.log("[PLAYER " + player.id + "]: " + server.players[playerID].id + " spawned");
             }
         }
         socket.on('updatePosition',(e) => {
