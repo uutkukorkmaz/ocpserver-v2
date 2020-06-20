44427f437d29527e05256a20438efc9579814336
uutkukorkmaz <uutkukorkmaz@gmail.com>
2020-06-20 04:02:06 +0300

    bug fix

--- System/Connection.js
+++ System/Connection.js
@@ -20,10 +20,9 @@ module.exports = class Connection {
         console.log("[SERVER]: "+player.id+" spawned ");
         socket.broadcast.emit('spawn', player);
         for(let playerID in server.players){
-            console.log (server.players[playerID].id != player.id);
             if(server.players[playerID].id != player.id) {
                 socket.emit('spawn', server.players[playerID]);
-                console.log("[" + player.id + "]: " + server.players[playerID].id + " spawned");
+                console.log("[PLAYER " + player.id + "]: " + server.players[playerID].id + " spawned");
             }
         }
 
--- System/Server.js
+++ System/Server.js
@@ -22,7 +22,7 @@ module.exports = class Server {
         //socket.join(this.connection.player.room);
         this.players[this.connection.player.id] = this.connection.player;
 
-        new Msg("Player " + this.connection.player.id + " joined to server.", "info");
+       // new Msg("Player " + this.connection.player.id + " joined to server.", "info");
         return this.connection;
     }
 
