b3bfe37a9c437df55b6816517fd5867e9bfd220a
uutkukorkmaz <uutkukorkmaz@gmail.com>
2020-06-20 04:27:17 +0300

    bug fix

--- System/Entities/Player.js
+++ System/Entities/Player.js
@@ -4,9 +4,10 @@ const IdGen = require('../IDGenerator');
 module.exports = class Player {
 
     constructor() {
+
         this.id = IdGen.generate()
         this.position = {
-            x: 0,
+            x: Math.floor(Math.random() * 10),
             y: 0,
             z: 0
         }
