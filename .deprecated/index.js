fcee461f035aee132e6b0b0bb523e5f34a0e8fef
uutkukorkmaz <uutkukorkmaz@gmail.com>
2020-07-01 12:05:23 +0300

    new server structure

---
+++ .deprecated/index.js
@@ -0,0 +1,20 @@
+const Server = require('./System/Server');
+const ServerConfig = require('./System/Config');
+const ConsoleMsg = require('./System/Msg');
+const chalk = require('chalk');
+let server = new Server();
+
+
+server.init();
+
+const SocketIO = require('socket.io')(ServerConfig.Config.Port);
+
+new ConsoleMsg("Server started on port "+chalk.inverse(" "+ServerConfig.Config.Port+" "));
+
+
+
+SocketIO.on('connection', (socket) => {
+    let connection = server.onConnected(socket);
+    socket.emit('register', {id: connection.player.id});
+    connection.createEvents();
+});
--- index.js
+++ index.js
@@ -1,20 +1,42 @@
-const Server = require('./System/Server');
-const ServerConfig = require('./System/Config');
-const ConsoleMsg = require('./System/Msg');
-const chalk = require('chalk');
-let server = new Server();
+const config = require('./lib/config')
+const SocketIO = require('socket.io')
+const chalk = require('chalk')
+const debug = require('./lib/debug')
+const Server = require('./lib/classes/Server')
+const Connection = require('./lib/classes/Connection')
+const Authentication = require('./lib/classes/Authentication')
+
+debug.processing('attempting to start server', "application")
+let io = new SocketIO(config.port, {
+    transports: ['websocket'],
+    upgrade: false
+})
+let server = new Server()
+debug.success('server started on port ' + chalk.bold(config.port))
+
+io.once('connection', (socket) => {
+    socket.emit('register', {id: socket.id})
+    debug.log("connection detected. waiting for the account credentials from socket " + socket.id)
+    let auth = new Authentication({username: "a", password: "a"}, socket);
+    let a = auth.auth().then((t) => {
+        if (typeof t != "undefined") {
+            //debug.log("account token: " + t.account.token)
+              let b = new Connection(t,server)
+
+        } else {
+            debug.error("wrong credentials")
+        }
+    })
+
+    socket.on('login', (credentials) => {
 
 
-server.init();
+    });
+    socket.on('positionUpdate', (data) => {
 
-const SocketIO = require('socket.io')(ServerConfig.Config.Port);
-
-new ConsoleMsg("Server started on port "+chalk.inverse(" "+ServerConfig.Config.Port+" "));
-
-
-
-SocketIO.on('connection', (socket) => {
-    let connection = server.onConnected(socket);
-    socket.emit('register', {id: connection.player.id});
-    connection.createEvents();
-});
+        console.log(socket.id)
+    })
+    socket.on('disconnect',()=>{
+        socket.socket.reconnect()
+    })
+})
