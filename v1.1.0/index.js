9a1b4c1704c4e8a243e04c623658f4ae98a01477
uutkukorkmaz <uutkukorkmaz@gmail.com>
2020-07-02 16:37:09 +0300

    new server structure

--- v1.1.0/index.js
+++ v1.1.0/index.js
@@ -22,7 +22,7 @@ io.on('connection', (socket) => {
         let auth = new Authentication(credentials, socket);
         let a = auth.auth().then((response) => {
             if (typeof response != "undefined") {
-                debug.log(response.account)
+                debug.log(JSON.stringify(response.account))
                 let b = new Connection(response,server)
 
             } else {
